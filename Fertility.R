a6e9c68445e7e448822d6e0dce87be028631ac44
Maggie Hodges <maggiehodges@users.noreply.github.com>
2017-11-28 11:50:07 -0800

    creating first two graphs

--- Fertility.R
+++ Fertility.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(readr)
 library(stringr)
 library(readxl)
+library(ggplot2)
 
 # get data
 fert <- read_csv("data/original_fertility_data.csv")
@@ -35,7 +36,6 @@ infert <- read_excel("original_infertility_data.xlsx", skip = 1, sheet=5, col_na
 second_infert <- read_excel("original_infertility_data.xlsx", skip = 1, sheet=6, col_names = FALSE)
      names(second_infert) <- c("iso3c","country","year", "total population women 20-44","secondary_infertility_rate")
 
-
 # Convert year to an integer column
 infert <- infert %>%
   mutate(year = as.integer(year))
@@ -53,8 +53,6 @@ fertility <- left_join(relevant_years_fert,infert, by = c("country", "year"))
 
 #combine fertility and secondary infertility data by countries
 secondary_infertility <- left_join(relevant_years_fert,second_infert, by = c("country", "year"))
-
-#combine 
 
 # organize data so it appears by country and year
 fertility <- fertility %>%
@@ -97,8 +95,8 @@ age_first_birth <- age_first_birth %>%
 # combine primary infertility and age at first birth data
 primary_infertility_age <- left_join(primary_infertility,age_first_birth, by = c("country"))
 
-# filter combined primary infertility and age at first birth data so it does 
-# not include countries which have null values, write to csv
+# filter combined primary infertility and age at first birth data so it writes null values,
+# as blank, write to csv
 write_csv(primary_infertility_age, "primary_infertility_age.csv", na="")
 
 # import health service coverage file
@@ -115,7 +113,6 @@ health_service_coverage <- health_service_coverage %>%
   filter(year == max(year)) %>%
   ungroup()
 
-
 # import health service coverage file
 health_service_coverage2 <- read_csv("health_service_coverage.csv") %>%
   select(1, 2, 3)
@@ -130,10 +127,6 @@ health_service_coverage2 <- health_service_coverage2 %>%
   filter(diff == min(diff)) %>%
   ungroup()
 
-
-
-
-
 #combine primary infertility and health service coverage data
 prim_infert_health_service_cov <- left_join(primary_infertility, health_service_coverage, by=c("country"))
 
@@ -147,8 +140,92 @@ health_service_coverage <- read_csv("health_service_coverage.csv") %>%
   select(1, 2, 3) %>%
   mutate(year = as.integer(year)) 
   
-#combine primary infertility and health service coverage data
+#combine secondary infertility and health service coverage data
   sec_infert_health_service_cov <- left_join(subset(secondary_infertility, year == 2010), health_service_coverage, by=c("country"))
   
 write_csv(sec_infert_health_service_cov, "sec_infert_health_service_cov.csv", na="")
+
+# import health service coverage file
+health_service_coverage2 <- read_csv("health_service_coverage.csv") %>%
+  select(1, 2, 3)
+
+names(health_service_coverage2) <- c("country","year","fp_met")
+
+# import health service infrastructure file
+health_infrastructure <- read_csv("health_infrastructure.csv") %>%
+  select(1, 2, 3, 4, 5)
+  
+names(health_infrastructure) <- c("country","year","hospitals", "health_posts", "health_centres")
+
+health_infrastructure <- health_infrastructure %>%
+  mutate(year = as.integer(year)) %>%
+  filter(year > 2010 & !is.na(health_centres)) %>%
+  mutate(diff = year - 2010) %>%
+  group_by(country) %>%
+  filter(diff == min(diff)) %>%
+  ungroup()
+
+#combine secondary infertility and health service coverage data
+sec_infert_health_infrastructure <- left_join(subset(secondary_infertility, year == 2010), health_infrastructure, by=c("country"))
+
+write_csv(sec_infert_health_infrastructure, "sec_infert_health_infrastructure.csv", na="")
+
+#combine primary and secondary infertility data
+prim_sec_infertility <- left_join(primary_infertility,secondary_infertility, by=c("country", "year")) %>%
+  filter(year == "2010") %>%
+  filter(!is.na(primary_infertility_rate)) %>%
+  filter(!is.na(secondary_infertility_rate))
+
+write_csv(prim_sec_infertility, "prim_sec_infertility.csv", na="")
+
+#Used excel to add in a total_infertility column, then re-imported data
+prim_sec_infertility <- read_csv("prim_sec_infertility.csv") 
+   names(prim_sec_infertility) <- c("iso2c", "country","year","gdp_percap", "child_mortality", "fertil_rate", "iso3c", "region", "capital", "longitude", "latitude", "income", "lending", "total population women aged 20-44", "primary_infertility_rate", "secondary_infertility_rate", "total_infertility")
+
+# create scatter plot of relationship between fertility and infertility
+prim_sec_infertility_chart <- ggplot(prim_sec_infertility, aes(x = fertil_rate, y = total_infertility, color = region)) + 
+  scale_color_brewer(palette = "Accent", name = "") +
+  geom_point(size = 1.5, alpha =0.6) +
+  theme_minimal(base_size = 12, base_family = "Georgia") +
+  xlab("Birth Rate") +
+  ylab("Infertility Rate") +
+  theme(legend.position = "bottom") 
+  
+# add a trend line
+prim_sec_infertility_chart +
+  geom_smooth(method = lm, se = FALSE, color = "black", linetype = "dotdash", size = 0.3) 
+
+#how I tried to make it interactive
+prim_sec_infertility_chart <- ggplot(prim_sec_infertility, aes(x = fertil_rate, y = total_infertility, color = region)) + 
+  scale_color_brewer(palette = "Accent", name = "") +
+  geom_point_interactive(size = 1.5, aes(tooltip = country)) +
+  theme_minimal(base_size = 12, base_family = "Georgia") +
+  xlab("Birth Rate") +
+  ylab("Infertility Rate") +
+  theme(legend.position = "bottom") 
+
+library(ggiraph)
+geom_point(size = 3, alpha = 0.6, aes(color = region)) +
+#stacked bar chart, primary and secondary infertility by region
+
+# import regional infertility average file
+prim_sec_region <- read_csv("prim_sec_infertility_region.csv") %>%
+  select(18, 19, 20, 21)
+
+names(prim_sec_region) <- c("prim_avg","sec_avg","total_avg", "region")
+
+write_csv(prim_sec_region, "prim_sec_infertility_region.csv", na="")
+
+prim_sec_stackedbar <- ggplot(prim_sec_region, aes(x = region, y = total_avg)) + 
+  scale_color_brewer(palette = "Accent", name = "") +
+  geom_bar(stat = "identity", 
+           color = "#888888", 
+           fill = "#CCCCCC", 
+           alpha = 0.5) +
+  theme_minimal(base_size = 12, base_family = "Georgia") +
+  xlab("Region") +
+  ylab("Infertility Rate") +
+  theme(legend.position = "bottom") +
+  ggtitle("Primary and Secondary Infertility by Region") +
+  coord_flip()
   
